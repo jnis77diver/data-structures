19edfb75b8d0f54dc10354c72bf480868fbf47c5
Jonah Nisenson <jsnisenson@gmail.com>
2015-02-07 12:52:21 -0500

    rewrite hashTable.js to make splice more readable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,3 +1,16 @@
+// This is a "hashing function". You don't need to worry about it, just use it
+// to turn any string into an integer that is well-distributed between the
+// numbers 0 and `max`
+var getIndexBelowMaxForKey = function(str, max){
+  var hash = 0;
+  for (var i = 0; i < str.length; i++) {
+    hash = (hash<<5) + hash + str.charCodeAt(i);
+    hash = hash & hash; // Convert to 32bit integer
+    hash = Math.abs(hash);
+  }
+  return hash % max;
+};
+
 var HashTable = function(){
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
@@ -5,18 +18,50 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var valueAtIndex = this._storage.get(i);
+  if( !valueAtIndex ) {
+    this._storage.set(i, [[k, v]]);
+  } else {
+    valueAtIndex.push([k,v]);
+    this._storage.set(i, valueAtIndex);
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  var valueAtIndex = this._storage.get(i);
+  if( valueAtIndex === undefined ) {
+    return null;
+  } else {
+    for( var j = 0; j < valueAtIndex.length; j++ ) {
+      if( valueAtIndex[j][0] === k ) {
+        return valueAtIndex[j][1];
+      }
+    }
+  }
+  return null;
 };
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  //set up a var and then get the value at i
+  var valueAtIndex = this._storage.get(i);
+  //check if undefined
+  if( valueAtIndex === undefined ) {
+    return null;
+  } else {
+    for( var j = 0; j < valueAtIndex.length; j++ ) {
+      if( valueAtIndex[j][0] === k ) {
+        var val = valueAtIndex[j];
+        var temp = j === 0? 1: j;
+        valueAtIndex.splice(j, temp);
+        this._storage.set(i, valueAtIndex);
+        return val;
+      }
+    }
+  }
+  return null;
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
